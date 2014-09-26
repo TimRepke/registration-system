b3d1a930e140399718d1905a203712364440b0f7
Tim Repke <repketim@informatik.hu-berlin.de>
2014-09-26 12:12:11 +0100

    now max number of registrations is editable

--- registration-system/admin/pages_infos.php
+++ registration-system/admin/pages_infos.php
@@ -21,11 +21,12 @@ if(isset($_POST['note-content'])){
                                  "map_pin"      => $_REQUEST['us2-lat']." ".$_REQUEST['us2-lon'],
                                  "leiter"       => $_REQUEST['leiter'],
                                  "kontakt"      => $_REQUEST['kontakt'],
+                                 "max_bachelor" => $_REQUEST['max_bachelor'],
                                  "regopen"      => $_REQUEST['regopen'] == "penis" ? 1 : 0],
                         array("fahrt_id"=>$config_current_fahrt_id));
 }
 
-$data = $admin_db->get("fahrten", ["beschreibung", "titel", "von", "bis", "ziel", "map_pin", "leiter", "kontakt", "regopen"], array("fahrt_id"=>$config_current_fahrt_id));
+$data = $admin_db->get("fahrten", ["beschreibung", "titel", "von", "bis", "ziel", "map_pin", "leiter", "kontakt", "regopen", "max_bachelor"], array("fahrt_id"=>$config_current_fahrt_id));
 
 $headers .="<!-- wysihtml5 parser rules -->
 <script src=\"../view/js/wysihtml5-0.3.0_rc2.min.js\"></script>
@@ -84,6 +85,8 @@ $text .= '
                     <input type="text" name="bis" id="bis" value="'.$data["bis"].'" /></li>
                 <li><label>Anm. offen</label>
                     <input type="checkbox" name="regopen" id="regopen" value="penis" '.(($data["regopen"]==1) ? "checked" : "").' /></li>
+                <li><label>Max TN</label>
+                    <input type="number" name="max_bachelor" id="max_bachelor" value="'.$data["max_bachelor"].'" /></li>
                 <li><label>Leiter</label>
                     <input type="text" name="leiter" id="leiter" value="'.$data["leiter"].'" /></li>
                 <li><label>E-Mail</label>
@@ -93,7 +96,7 @@ $text .= '
         <div style="float:left">
             <label>Map Pin</label>
                 Location: <input type="text" id="us2-address" style="width: 200px"/>
-                <div id="us2" style="width: 500px; height: 315px;"></div>
+                <div id="us2" style="width: 500px; height: 353px;"></div>
                 <input type="hidden" id="us2-lat" name="us2-lat" value="'.explode(" ",$data["map_pin"])[0].'" />
                 <input type="hidden" id="us2-lon" name="us2-lon" value="'.explode(" ",$data["map_pin"])[1].'" />
                 <script>
