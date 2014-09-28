e91a68e884a532ead6b3bb0e9165aeaee4d33b87
Tim Repke <repketim@informatik.hu-berlin.de>
2014-09-28 23:49:27 +0100

    verboselevel turned off | in admin panel - list - now popup for editing included, not working yet though

--- registration-system/admin/pages_list.php
+++ registration-system/admin/pages_list.php
@@ -7,7 +7,7 @@
  */
 
 
-global $text, $headers, $admin_db, $config_current_fahrt_id, $ajax;
+global $text, $headers, $admin_db, $config_current_fahrt_id, $ajax, $config_studitypen, $config_essen, $config_reisearten;
 
 if(isset($_REQUEST['ajax'])){
 
@@ -15,9 +15,72 @@ if(isset($_REQUEST['ajax'])){
         $col = $_REQUEST['update'];
         $id  = $_REQUEST['hash'];
         $val = ($_REQUEST['nstate'] == 1) ? time() : NULL;
-
         $admin_db->update("bachelor", array($col=>$val), array("bachelor_id"=> $id));
     }
+
+
+    elseif(isset($_REQUEST['form'])){
+        $bid = $_REQUEST['hash'];
+
+        $ecols = [
+            "forname",
+            "sirname",
+            "mehl",
+            "pseudo",
+            "antyp",
+            "abtyp",
+            "anday",
+            "abday",
+            "comment",
+            "studityp"
+        ];
+        $rcols = [
+            "bachelor_id" => ["Hash",   function( $b ){ return $b; }],
+            "fahrt_id" => ["Fahrt",     function( $b ){ return "ID ".$b; }],
+            "anm_time" => ["Anmeldung", function( $b ){ return date("d.m.Y",$b); }],
+            "paid"     => ["Bezahlt",   function( $b ){ return ($b==0) ? "Nein" : date("d.m.Y", $b); }],
+            "repaid"   => ["Rückgezahlt", function($b){ return ($b==0) ? "Nein" : date("d.m.Y", $b); }],
+            "backstepped" => ["Zurückgetreten", function( $b ){ return ($b==0) ? "Nein" : date("d.m.Y", $b); }]
+        ];
+
+
+        $bachelor = $admin_db->get('bachelor', array_merge($ecols, array_keys($rcols)), array('bachelor_id'=>$bid));
+        $possible_dates = comm_get_possible_dates($admin_db, $bachelor['fahrt_id']);
+
+        foreach($rcols as $k=>$r){
+            $ajax .= "<b>".$r[0].":</b> ".$r[1]($bachelor[$k])."<br />";
+        }
+
+        $ajax .= '<br />
+        <div id="stylized" class="myform">
+        <form id="form" name="form" method="post" action="">';
+
+        $ajax .= admin_show_formular_helper_input("Vorname", "forname", $bachelor["forname"], "");
+        $ajax .= admin_show_formular_helper_input("Nachname","sirname",$bachelor["sirname"],"");
+        $ajax .= admin_show_formular_helper_input("Anzeigename","pseudo",$bachelor["pseudo"],"");
+        $ajax .= admin_show_formular_helper_input("E-Mail-Adresse","mehl",$bachelor["mehl"],"regelmäßig lesen!");
+        $ajax .= admin_show_formular_helper_sel("Du bist","studityp",$config_studitypen, $bachelor["studityp"],"");
+        $ajax .= admin_show_formular_helper_sel("Alter 18+?","virgin",array("Nein", "Ja"), (($bachelor["virgin"]==1) ? "Nein" : "Ja"), "Bist du älter als 18 Jahre?");
+        $ajax .= admin_show_formular_helper_sel("Essenswunsch","essen",$config_essen, $bachelor["essen"],"Info für den Koch.");
+        $ajax .= admin_show_formular_helper_sel2("Anreise","anday", array_slice($possible_dates,0, -1), $bachelor["anday"]
+            ,"antyp",$config_reisearten, $bachelor["antyp"],"");
+        $ajax .= admin_show_formular_helper_sel2("Abreise","abday", array_slice($possible_dates,1), $bachelor["abday"]
+            ,"abtyp",$config_reisearten,$bachelor["abtyp"],"");
+        $ajax .= '
+        <label>Anmerkung</label>
+        <textarea id="comment" name="comment" rows="3" cols="40">'.$bachelor["comment"].'</textarea>
+        <input type="checkbox" name="public" value="public" style="width:40px" '.(($bachelor['public']==1 ? " checked" : "")).'><span style="float:left">Anmeldung verstecken</span><br/>
+        <div style="clear:both"></div>
+
+        <button type="submit" name="submit" id="submit" value="submit">Ändern!</button>
+        <div class="spacer"></div>';
+
+
+        $ajax .= '</form>
+        </div>
+        ';
+    }
+
 
 } else {
 $headers =<<<END
@@ -61,11 +124,32 @@ div.btn{
     top:100px;
     left: 200px;
     width: 700px;
-    height: 800px;
+    height: 700px;
     overflow: auto;
     border: 1px solid #000000;
-    background-color: #b0bed9;
+    background-color: beige;
+    padding: 20px 10px 10px 10px;
 }
+
+#editFormTopbar{
+    background-color: #b0bed9;
+    height: 20px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    paddin: 0;
+}
+#editFormTopbar p{
+    position: absolute;
+    float: right;
+    top: 0;
+    padding: 0;
+    margin: 0;
+    right: 5px;
+    cursor: none;
+}
+
 </style>";
 
 $text .= "Meldeliste";
@@ -90,7 +174,7 @@ $columns = array(
 );
 
 $columnFunctions = array(
-    "Anmelde-ID" => function($person) { return $person["bachelor_id"]; }
+    "Anmelde-ID" => function($person) { return "<a href='#' class='edit_bachelor'>".$person["bachelor_id"]."</a>"; }
     //,"FahrtID" => function($person) { return $person["fahrt_id"]; }
 ,"Anmeldung" => function($person) { return date("d.m.Y", $person['anm_time']); },
     "Name" => function($person) { return "<a href='mailto:".$person["mehl"]."?subject=FS-Fahrt'>".$person["forname"]." ".$person["sirname"]." (".$person["pseudo"].")</a>"; },
@@ -131,7 +215,10 @@ foreach($people as $person) {
 $text .=<<<END
         </tbody>
     </table>
-    <div id="editForm"></div>
+    <div id="editForm">
+        <div id="editFormTopbar"><p>X</p></div>
+        <p></p>
+    </div>
     <script type='text/javascript'>
 
         jQuery.extend( jQuery.fn.dataTableExt.oSort, {
@@ -159,6 +246,7 @@ $text .=<<<END
                 "iDisplayLength": 70,
                 "columnDefs": [
                     { type: 'link', targets: 2 },
+                    { type: 'link', targets: 0 },
                     { type: 'prb', targets: 9 }
                 ],
                 "aoColumnDefs": [
@@ -173,7 +261,7 @@ $text .=<<<END
                                 var txt = data[9].split(",");
                                 for(var i = 0; i < txt.length; i++){
                                     var tmp = (txt[i]==0) ? 0 : 1;
-                                    btns += "<div onclick=\"btnclick(this, '"+classes[i]+"','"+row[0]+"',"+tmp+");\" class='btn btn-"+classes[i]+"-"+tmp+"'>&nbsp;</div>";
+                                    btns += "<div onclick=\"btnclick(this, '"+classes[i]+"','"+data[0].match(/<a [^>]+>([^<]+)<\/a>/)[1]+"',"+tmp+");\" class='btn btn-"+classes[i]+"-"+tmp+"'>&nbsp;</div>";
                                 }
 
                                 // Store the computed display for speed
@@ -189,6 +277,37 @@ $text .=<<<END
                     }
                 ],
                 "order": [[ 2, "asc" ]]
+            });
+
+            $(".edit_bachelor").click( function(){
+                var bid = $(this).text();
+                $.get( "?page=list&ajax=ajax&form=form&hash="+bid, function( data ) {
+                    $("#editForm > p").html(data);
+                });
+
+                $("#editForm").show();
+            });
+
+            $("#editFormTopbar > p").click( function(){
+                $(this).parent().parent().hide();
+            });
+
+            $(".js-ajax-php-json").submit(function(){
+                var data = $(this).serialize();
+                $.ajax({
+                    type: "POST",
+                    dataType: "json",
+                    url: "response.php",
+                    data: data,
+                    success: function(data) {
+                        $(".the-return").html(
+                            "Favorite beverage: " + data["favorite_beverage"] + "<br />Favorite restaurant: " + data["favorite_restaurant"] + "<br />Gender: " + data["gender"] + "<br />JSON: " + data["json"]
+                        );
+
+                        // alert("Form submitted successfully.Returned json: " + data["json"]);
+                    }
+                });
+                return false;
             });
         });
 
