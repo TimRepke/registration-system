77b3f078716bbe909755ff005b02ba6fdfe165f2
Tim Repke <repketim@informatik.hu-berlin.de>
2014-10-05 17:18:29 +0100

    now receipt table is working, too

--- registration-system/admin/pages_cost/pages_cost.js
+++ registration-system/admin/pages_cost/pages_cost.js
@@ -171,10 +171,96 @@
         };
     });
 
-    app.controller('TableReceiptController', function(){
+    app.controller('TableReceiptController', ["$scope", "$filter", "$q", "$http", function($scope, $filter, $q, $http){
         var table = this;
 
-    });
+        table.entries = [];
+
+        $http.get('?page=cost&ajax=get-receipt-json').success(function(data){
+            if(data !== "")
+                table.entries = data;
+        });
+
+        // === basic table functions ===
+
+        table.rowSum = function(row){
+            return row.cnt*row.mul*row.price;
+        };
+
+        table.sum = function(){
+            var ret = 0;
+            for(var run = 0; run < table.entries.length; run++)
+                if(!table.entries[run].isDeleted)
+                    ret += table.rowSum(table.entries[run]);
+            return ret;
+        };
+
+        // === edit table functions ===
+
+        // filter rows to show
+        $scope.filterRow = function(row) {
+            return row.isDeleted !== true;
+        };
+
+        // mark row as deleted
+        $scope.deleteRow = function(index) {
+            table.entries[index].isDeleted = true;
+        };
+
+        $scope.chang = function(index, prop, dat){
+            table.entries[index][prop] = dat;
+        };
+
+        // add row
+        $scope.addRow = function() {
+            table.entries.push({
+                pos: "",
+                cnt: 1,
+                mul: 1,
+                price: 0,
+                isNew: true
+            });
+        };
+
+        // cancel all changes
+        $scope.cancel = function() {
+            for (var i = table.entries.length; i--;) {
+                var row = table.entries[i];
+
+                // undelete
+                if (row.isDeleted) {
+                    delete row.isDeleted;
+                }
+                // remove new
+                if (row.isNew) {
+                    table.entries.splice(i, 1);
+                }
+            }
+        };
+
+        // save edits
+        $scope.saveTable = function() {
+            var results = [];
+            for (var i = table.entries.length; i--;) {
+                var row = table.entries[i];
+
+                // actually delete row
+                if (row.isDeleted) {
+                    table.entries.splice(i, 1);
+                }
+                // mark as not new
+                if (row.isNew) {
+                    delete row.isNew;
+                }
+            }
+
+            $http.post('?page=cost&ajax=set-receipt-json', table.entries).success(function(data, status, headers, config){
+                toastr.success('Saved to Database!')
+            });
+            return $q.all(results);
+        };
+
+     }]);
 
 
 })();
