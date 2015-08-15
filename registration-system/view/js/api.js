5f7d3859c31783bc1fb2a2a7a8e986261836829e
Tim Repke <timmothey@gmx.de>
2015-08-15 18:01:22 +0200

    added api body

---
+++ registration-system/view/js/api.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * params will contain the attributes to send (as an object)
+ *   -> will be defined later
+ *
+ * callback is called, once the request was sent to the server.
+ * It'll transmit an object containing the
+ *    -> state (0 = successful, 1 = error, 2 = ?)
+ *    -> main message (string)
+ *    -> errors (null, if non; array of strings with messages else)
+ *
+ * @param params
+ * @param callback
+ */
+function api_send_signup(params, callback) {
+
+    // TODO evaluate params and send them
+
+    callback({
+        state: 0,
+        messages: 'Successful signup',
+        errors: null
+    })
+}
