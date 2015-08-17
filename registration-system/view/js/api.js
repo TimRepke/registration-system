c759154cc8da149f774e8d71a275ec57c6f03210
Tim Repke <timmothey@gmx.de>
2015-08-17 21:33:27 +0200

    added first attempt for frontend API prototype; added testscript

--- registration-system/view/js/api.js
+++ registration-system/view/js/api.js
@@ -1,3 +1,97 @@
+var UrlComponents = {
+    decomposed: null,
+    getParams: function() {
+        if(this.decomposed) return this.decomposed;
+
+        var query = location.search.substr(1);
+        this.decomposed = decomposer(query);
+        return this.decomposed;
+
+        function decomposer(q) {
+            // taken from http://jsperf.com/querystring-with-javascript
+            return (function(a) {
+                if (a == "") return {};
+                var b = {};
+                for (var i = 0; i < a.length; ++i) {
+                    var p = a[i].split('=');
+                    if (p.length != 2) continue;
+                    b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+                }
+                return b;
+            })(q.split("&"));
+        }
+    },
+    getValueOf: function(param) {
+        return this.getParams()[param];
+    }
+};
+
+function Bachelor() {
+    var properties = {
+        'forname': null,
+        'sirname': null,
+        'anday':   null,
+        'abday':   null,
+        'antyp':   null,
+        'abtyp':   null,
+        'pseudo':  null,
+        'mehl':    null,
+        'essen':   null,
+        'public':  null,
+        'studityp':null,
+        'comment': null
+    };
+
+    this.getProperties = function () {
+        return properties;
+    };
+
+    this.getValue = function (attribute) {
+        if(attribute in properties)
+            return properties[attribute];
+        throw new Error("Attribute does not exist!");
+    };
+
+    this.isSet = function (attribute) {
+        return !!properties[attribute];
+    };
+
+    this.setValue = function (attribute, value) {
+        if(attribute in properties)
+            properties[attribute] = value;
+        else
+            throw new Error("This property isn't supposed to be here!");
+    };
+
+    this.resetValue = function (attribute) {
+        if(attribute in properties)
+            properties[attribute] = null;
+        else
+            throw new Error("This property isn't supposed to exist in the first place!");
+    };
+
+    this.setValues = function (props) {
+        if(props) {
+            for(var key in props){
+                this.setValue(key, props[key]);
+            }
+        }
+    };
+
+    this.testValidValue = function (attribute, value) {
+        //TODO should return true, if the given value is valid for the given attribute (soft protect)
+    };
+}
+
+function FAPI() {
+    var bachelor = new Bachelor();
+    this.methodBasepath = 'view/signups/' + UrlComponents.getValueOf('method') + '/';
+}
+
+FAPI.prototype.resolvePath = function(file) {
+    return this.methodBasepath + file;
+};
+
 /**
  *
  * params will contain the attributes to send (as an object)
@@ -33,3 +127,4 @@ function api_soft_protect(elementIds, regex) {
         });
     }
 }
+
