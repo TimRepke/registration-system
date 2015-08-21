01da57c5ae311f168d2a00f37d8fa2bd71a70b50
Tim Repke <timmothey@gmx.de>
2015-08-21 14:10:30 +0200

    switched to use fapi to point to ressources

--- registration-system/view/signups/game1/js/game.js
+++ registration-system/view/signups/game1/js/game.js
@@ -4,28 +4,17 @@ function Game(config) {
 	Game.config = config;
 	Game.instance = this;
 }
-Game.eventLayers = ['CLICKABLE', 'WALK', 'NOWALK', 'EVENT'];
 Game.prototype.run = function() {
-	d3.xml('maps/'+Game.config.startMap, 'image/svg+xml', function(xml) {
+	d3.xml(FAPI.resolvePath('maps/map_landing.svg'), 'image/svg+xml', function(xml) {
 		var gameCanvas = document.getElementById("gameCanvas");
 		var gameRoot = document.getElementById("gameRoot");
 		gameCanvas.style.width = Game.config.size[0]+'px';
 		gameCanvas.style.height = Game.config.size[1]+'px';
 		gameRoot.appendChild(xml.documentElement);
-
+	
 		var svg = d3.select("svg");
-
-		var displayEvents = Game.config.showEventLayers ? 'block' : 'none';
-		svg.selectAll('g').filter(function() {
-			return (
-				this.getAttribute('inkscape:groupmode') == 'layer'
-			 && Game.eventLayers.indexOf(this.getAttribute('inkscape:label')) >= 0
-			);
-		}).style('display', displayEvents);
-
 		var char = new Char(svg);
 		var cam = new Camera(svg, char.translation);
-
 	
 		// test animation
 		var ship = svg.select("#shipGroup");
@@ -37,13 +26,11 @@ Game.prototype.run = function() {
 	
 		// animate
 		setInterval(function() {
-			if (char.loaded) {
-				// move player
-				char.physics();
-				char.animate();
-				// cam movement
-				cam.movement();
-			}
+			// move player
+			char.physics();
+			char.animate();
+			// cam movement
+			cam.movement();
 		}, 10);
 	
 		svg.on("click", function(d) {
