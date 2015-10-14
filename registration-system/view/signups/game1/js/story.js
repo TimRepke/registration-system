1b09f86bb033f7fecd7d26a7e3df94e493bfff00
Manuel Herrmann <it@icetruck.de>
2015-10-14 13:30:56 +0200

    animate fix

--- registration-system/view/signups/game1/js/story.js
+++ registration-system/view/signups/game1/js/story.js
@@ -194,6 +194,7 @@ Story.actions = {
                         condition: Environment.progress.inventory_money,
                         action: function () {
                             Environment.progress.inventory_ruestung = true;
+							if (Game.char) Game.char.animate(); // apply new visuals
                         }
                     }, {
                         message: 'Was? Spende? Ich hab\' nichts!',
