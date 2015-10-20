5be1f6fcd9c909470352d8f452facdf6877be632
Tim Repke <timmothey@gmx.de>
2015-10-21 01:57:20 +0200

    some gitignore additions, cleaned local config

--- registration-system/config.local.php
+++ registration-system/config.local.php
@@ -4,17 +4,16 @@ $config_verbose_level = 0; // 0 = nothing, 1 = important, 2 = somewhat important
 $config_admin_verbose_level = 0;
 
 // URL where site is hosted with trailing slash
-$config_baseurl = "http://localhost/fsfahrt"; //"http://fsfahrt.repke.eu/anmeldung/registration-system/";
+$config_baseurl = "http://localhost/fsfahrt";
 
 // absolute path to doc root withOUT trailing slash
-$config_basepath = __DIR__; //"/var/www/vhosts/fsfahrt.repke.eu/httpdocs/anmeldung/registration-system";
+$config_basepath = __DIR__;
 
 // database config
-$config_db = array(
+$config_db = [
     "name" => "fsfahrt",    // name of DB
     "user" => "fsfahrt",    // username
     "pass" => "9Lug*96q",   // password
     "host" => "localhost",  // host
     "type" => "mysql"       // type of DB - only tested with mysql (so better not change)!!
-);
-
+];
