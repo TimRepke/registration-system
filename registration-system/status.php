d3df4363ed16e77f01b7d5d8e676a5a5fba01484
Tim Repke <timmothey@gmx.de>
2016-10-15 18:40:20 +0200

    refactored status page

--- registration-system/status.php
+++ registration-system/status.php
@@ -1,78 +1,119 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-require 'config.inc.php';
-require 'frameworks/medoo.php';
-require 'frameworks/commons.php';
-require 'lang.php';
+require_once __DIR__.'/view/default_index.php';
+require_once __DIR__.'/frameworks/Fahrt.php';
+require_once __DIR__.'/frameworks/Bachelor.php';
 
-$config_verbose_level = 0;
+class Status extends DefaultIndex {
 
-$status_db = new medoo(array(
-    'database_type' => $config_db["type"],
-    'database_name' => $config_db["name"],
-    'server'        => $config_db["host"],
-    'username'      => $config_db["user"],
-    'password'      => $config_db["pass"]
-));
+    private $hash;
+    private $bachelor;
+    private $fahrt;
 
-require 'view/default_index.php';
+    public function __construct() {
+        parent::__construct();
+        $this->hash = (isset($_REQUEST['hash'])) ? $_REQUEST['hash'] : null;
 
+        $tripID = $this->environment->getSelectedTripId();
+        if (is_null($tripID))
+            $tripID = $this->environment->getCurrentTripId();
 
-function show_content(){
-    global $status_db, $config_current_fahrt_id, $config_mailtag;
-    if(!isset($_REQUEST['hash']))
-        die("Kein Hash angegeben!");
-
-    $data = $status_db->get("bachelor", "*", array("bachelor_id"=>substr($_REQUEST['hash'],0,15)));
-    $wl = FALSE;
-    if(!$data){
-        $data = $status_db->get("waitlist", "*", array("waitlist_id"=>substr($_REQUEST['hash'],0,15)));
-        $wl = TRUE;
-        if(!$data)
-            die("<h1>Kein gültiger Hash gegeben!</h1>");
+        $this->fahrt = new Fahrt($tripID);
+        $this->bachelor = Bachelor::makeFromDB($this->fahrt, $this->hash);
     }
 
-    $infolist = Array(
-    	'Anmelde ID' => $data['bachelor_id'],
-        'Anmeldetag' => date('d.m.Y', $data['anm_time']),
-    	'Vor-/Nachname' => $data['forname'].' '.$data['sirname'].(strlen($data['pseudo']) > 0 ? ' ('.$data['pseudo'].')' : ""),
-    	'eMail-Adresse' => $data['mehl'],
-    	'Anreisetag &amp; Art' => comm_from_mysqlDate($data["anday"]).' ('.$data["antyp"].')',
-    	'Abreisetag &amp; Art' => comm_from_mysqlDate($data["abday"]).' ('.$data["abtyp"].')',
-    	'Essenswunsch' => $data["essen"],
-    	'Zahlung erhalten' => ((is_null($data["paid"])) ? "nein" : date('d.m.Y',$data["paid"])),
-    	'Rückzahlung gesendet' => ((is_null($data["repaid"])) ? "nein" : date('d.m.Y',$data["repaid"])),
-    	//'Zurückgetreten' => (($data["backstepped"]==1) ? "ja" : "nein"),
-    	'Kommentar' => $data["comment"]
-    );
+    private function assertDataAvailable() {
+        if (is_null($this->fahrt) or is_null($this->bachelor)) {
+            echo '<h2>Fehler</h2> 
+                  Es wurde kein, oder nur ein ungültiger Hash angegeben. <br />
+                  Auf jeden Fall konnten keine Daten gefunden werden!';
+            return false;
+        }
+        return true;
+    }
 
-    echo '
-    <div class="fahrt" style="background: #f9f9f9"><div class="fahrttitle">Anmeldedaten</div>';
+    private function displayWarningIfNeeded() {
+        $msgs = [];
+        if ($this->bachelor->isBackstepped())
+            array_push($msgs, 'Achtung, die Anmeldung wurde ungültig gemacht!<br />
+                              Sofern keine weiteren Auskünfte folgen, kannst du leider NICHT mitfahren...');
 
-    if($wl)
-        echo '<div style="color: red; font-weight: bold; font-size: 14pt;">Achtung, dies ist nur ein Eintrag auf der Warteliste!<br /> Sofern keine weiteren Auskünfte folgen, kannst du leider NICHT mitfahren...</div>';
+        if ($this->bachelor->isWaiting())
+            array_push($msgs, 'Achtung, dies ist nur ein Eintrag auf der Warteliste!<br />
+                               Sofern keine weiteren Auskünfte folgen, kannst du leider NICHT mitfahren...');
 
-    if($data["backstepped"]==1)
-        echo '<div style="color: red; font-weight: bold; font-size: 14pt;">Achtung, die Anmeldung wurde ungültig gemacht!<br /> Sofern keine weiteren Auskünfte folgen, kannst du leider NICHT mitfahren...</div>';
+        foreach ($msgs as $msg) {
+            echo '<div style="color: red; font-weight: bold; font-size: 14pt;margin-bottom: 1em;">' . $msg . '</div>';
+        }
+    }
 
-    echo'
-    <div class="fahrttable">';
+    private function getTransformedData() {
+        $data = $this->bachelor->getData();
 
-    foreach($infolist as $key => $value)
-    {
-    	echo '<div>'; // (invisible(magic(style))) table row
-       	echo "<div style='display:table-cell; font-weight: bold; padding: 3px 40px 3px 0'>$key</div><div style='display:table-cell'>$value</div>";
+        return [
+            'Anmelde ID' => $data['bachelor_id'],
+            'Anmeldetag' => date('d.m.Y', $data['anm_time']),
+            'Vor-/Nachname' => $data['forname'] . ' ' . $data['sirname'] . (strlen($data['pseudo']) > 0 ? ' (' . $data['pseudo'] . ')' : ''),
+            'eMail-Adresse' => $data['mehl'],
+            'Anreisetag &amp; Art' => comm_from_mysqlDate($data['anday']) . ' (' . $data['antyp'] . ')',
+            'Abreisetag &amp; Art' => comm_from_mysqlDate($data['abday']) . ' (' . $data['abtyp'] . ')',
+            'Essenswunsch' => $data['essen'],
+            'Zahlung erhalten' => ((is_null($data['paid'])) ? 'nein' : date('d.m.Y', $data['paid'])),
+            'Rückzahlung gesendet' => ((is_null($data['repaid'])) ? 'nein' : date('d.m.Y', $data['repaid'])),
+            //'Zurückgetreten' => (($data['backstepped']==1) ? 'ja' : 'nein'),
+            'Kommentar' => $data['comment']
+        ];
+    }
+
+    private function displayTable() {
+        $infolist = $this->getTransformedData();
+
+        echo '<div class="fahrttable">';
+
+        foreach ($infolist as $key => $value) {
+            echo '<div>';
+            echo '<div style="display: table-cell; font-weight: bold; padding: 3px 40px 3px 0">' . $key . '</div>';
+            echo '<div style="display: table-cell">' . $value . '</div>';
+            echo '</div>';
+        }
         echo '</div>';
     }
-    echo '</div></div>';
 
-    $mailto = $status_db->get("fahrten", "kontakt", array("fahrt_id"=>$config_current_fahrt_id));
-    $subject= $config_mailtag.'Änderung zu '.$data["forname"].' '.$data["sirname"].' ('.$data["pseudo"].')';
+    private function displayMailLink() {
+        $fdata = $this->fahrt->getFahrtDetails();
+        $bdata = $this->bachelor->getData();
 
-    echo '<a style="float:none;font-weight:bold" href="mailto:'.$mailto.'?subject='.str_replace(" ", "%20",$subject).'">Änderung melden</a>';
+        $subject = $this->environment->sysconf['mailTag'] . ' Änderung zu ' .
+            $bdata['forname'] . ' ' . $bdata['sirname'] . ' (' . $bdata['pseudo'] . ')';
 
+        echo 'Fehler entdeckt? Dann schreib eine Mail an ' . $fdata['leiter'] . ' (' . $fdata['kontakt'] . '): ';
+        echo '<a style="float:none;font-weight:bold"
+                 href="mailto:' . $fdata['kontakt'] . '?subject=' . str_replace(" ", "%20", $subject) . '">Änderung melden</a>';
+    }
+
+    private function displayFahrtInfo() {
+        $fdata = $this->fahrt->getFahrtDetails();
+        echo 'Angaben für ' . $fdata['titel'] .
+             ' ('.$this->mysql2german($fdata['von']).' - '.$this->mysql2german($fdata['bis']).')';
+    }
+
+    protected function echoHeaders() {
+        echo '<link rel="stylesheet" href="view/style.css" />';
+    }
+
+    protected function echoContent() {
+        echo '<div class="fahrt" style="background: #f9f9f9"><div class="fahrttitle">Anmeldedaten</div>';
+        if ($this->assertDataAvailable()) {
+            $this->displayFahrtInfo();
+            $this->displayWarningIfNeeded();
+            $this->displayTable();
+            $this->displayMailLink();
+        }
+        echo '</div>';
+    }
 }
 
-function echo_headers() {
-    echo '<link rel="stylesheet" href="view/style.css" />';
-}
+(new Status())->render();
