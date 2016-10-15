c00c2c154943258aa75eb0906314d9ef5bb42042
Tim Repke <timmothey@gmx.de>
2016-10-15 23:11:07 +0200

    add impressum

--- registration-system/config.local.php
+++ registration-system/config.local.php
@@ -4,16 +4,18 @@ $config_verbose_level = 0; // 0 = nothing, 1 = important, 2 = somewhat important
 $config_admin_verbose_level = 0;
 
 // URL where site is hosted with trailing slash
-$config_baseurl = "http://localhost/fsfahrt";
+$config_baseurl = "http://localhost:8080/";
 
 // absolute path to doc root withOUT trailing slash
 $config_basepath = __DIR__;
+
+$config_impressum = 'https://fachschaft.informatik.hu-berlin.de/index.php/Fachschaft_Informatik:Impressum';
 
 // database config
 $config_db = [
     "name" => "fsfahrt",    // name of DB
     "user" => "fsfahrt",    // username
     "pass" => "9Lug*96q",   // password
-    "host" => "localhost",  // host
+    "host" => "db",  // host
     "type" => "mysql"       // type of DB - only tested with mysql (so better not change)!!
 ];
