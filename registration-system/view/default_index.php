a42172d8c8cdbd70513722bba120bea1e5d3d764
Tim Repke <timmothey@gmx.de>
2016-10-15 18:40:05 +0200

    default template now handles like an abstract class

--- registration-system/view/default_index.php
+++ registration-system/view/default_index.php
@@ -1,54 +1,75 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
-<head>
+<?php
 
-    <!-- ------------------------------------------------------- -->
-    <!--               Programmiert von                          -->
-    <!--                                                         -->
-    <!--               ~ Manu Herrmann ~                         -->
-    <!--                     und                                 -->
-    <!--                ~ Tim Repke ~                            -->
-    <!--                                                         -->
-    <!-- Erste Version in 2014.                                  -->
-    <!-- Beide haben 2015 noch einmal eine Schippe draufgesetzt! -->
-    <!-- ------------------------------------------------------- -->
+require_once __DIR__.'/../frameworks/Environment.php';
 
-    <!--
-         Die Entwickler sind der Meinung, dass im Quelltext rumschnüffeln unethisch ist.
-         Da wir sehr schlau sind, haben wir deinen Rechner infiziert. Eine Anmeldung zur
-         Fachschaftsfahrt ist daher nicht mehr möglich.
-         Selbst schuld!
+abstract class DefaultIndex {
 
-         PS: Solltest du es doch geschafft haben, gib' uns bitte Bescheid, damit wir dich wieder löschen.
-    -->
+    protected $environment;
 
-    <title>Fachschaftsfahrt</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <?php
+    protected function __construct() {
+        $this->environment = Environment::getEnv();
+    }
 
-    echo_headers();
+    abstract protected function echoHeaders();
+    abstract protected function echoContent();
 
-    ?>
-</head>
-<body>
-<div class="shadowbox">
-    <div id="headerbox">
-        <div class="headerboxshade"><h1>Fachschaftsfahrt</h1></div>
-        <div class="headerboxshade"><h2>Informatik</h2></div>
-        <p></p>
-    </div>
-    <div id="menubox">
+    protected function resolvePath($resource) {
+        return $this->environment->sysconf['baseURL'] . 'view/' . $resource;
+    }
 
-        <?php
-        show_content();
+    protected function mysql2german($date) {
+        return date('d.m.Y', strtotime($date));
+    }
+
+    public function render() {
         ?>
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+        <head>
 
-    </div>
-    <div id="footerbox">&copy;<?php echo date("Y"); ?> Fachschaftsinitiative Informatik der Humboldt Universität zu
-        Berlin.
-    </div>
-    <img id="nyan" alt="O" src="view/graphics/studityp_5.gif" style="position: fixed;bottom: 5px;left:0px"/>
+            <!-- ------------------------------------------------------- -->
+            <!--               Programmiert von                          -->
+            <!--                                                         -->
+            <!--               ~ Manu Herrmann ~                         -->
+            <!--                     und                                 -->
+            <!--                ~ Tim Repke ~                            -->
+            <!--                                                         -->
+            <!-- Erste Version in 2014.                                  -->
+            <!-- Beide haben 2015 noch einmal eine Schippe draufgesetzt! -->
+            <!-- Irgendwie 2016 noch einmal!                             -->
+            <!-- ------------------------------------------------------- -->
 
-</div>
-</body>
-</html>
+            <!--
+                 Die Entwickler sind der Meinung, dass im Quelltext rumschnüffeln unethisch ist.
+                 Da wir sehr schlau sind, haben wir deinen Rechner infiziert. Eine Anmeldung zur
+                 Fachschaftsfahrt ist daher nicht mehr möglich.
+                 Selbst schuld!
+
+                 PS: Solltest du es doch geschafft haben, gib' uns bitte Bescheid, damit wir dich wieder löschen.
+            -->
+
+            <title>Fachschaftsfahrt</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+            <?php $this->echoHeaders(); ?>
+        </head>
+        <body>
+            <div class="shadowbox">
+                <div id="headerbox">
+                    <div class="headerboxshade"><h1>Fachschaftsfahrt</h1></div>
+                    <div class="headerboxshade"><h2>Informatik</h2></div>
+                    <p></p>
+                </div>
+                <div id="menubox">
+                    <?php $this->echoContent(); ?>
+                </div>
+                <div id="footerbox">&copy;<?php echo date("Y"); ?>
+                    Fachschaftsinitiative Informatik der Humboldt Universität zu Berlin.
+                </div>
+                <img id="nyan" alt="O" src="<?php echo $this->resolvePath('graphics/studityp_5.gif') ?>"
+                     style="position: fixed;bottom: 5px;left:0"/>
+            </div>
+        </body>
+        </html>
+    <?php
+    }
+}
