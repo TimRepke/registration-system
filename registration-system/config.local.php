2f82cdd39ecdcee68660fe70832945526a3f15d4
Tim Repke <timmothey@gmx.de>
2016-10-16 23:44:21 +0200

    add ability for db log dump

--- registration-system/config.local.php
+++ registration-system/config.local.php
@@ -2,6 +2,7 @@
 
 $config_verbose_level = 0; // 0 = nothing, 1 = important, 2 = somewhat important, 3 = detailed verbose, 4 = with sql
 $config_admin_verbose_level = 0;
+$config_databse_debug = false;
 
 // URL where site is hosted with trailing slash
 $config_baseurl = "http://localhost:8080/";
