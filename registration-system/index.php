6a8ff230399791232958c9b77368ddf364431c4f
Tim Repke <timmothey@gmx.de>
2016-10-17 01:10:50 +0200

    remove error reporting

--- registration-system/index.php
+++ registration-system/index.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once __DIR__ . '/frameworks/Environment.php';
 require_once __DIR__ . '/view/default_index.php';
 require_once __DIR__ . '/view/signups/index.php';
