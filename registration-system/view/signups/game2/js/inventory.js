929959e82f7ab56a82cfc6924c6edc33e8818d74
Tim Repke <timmothey@gmx.de>
2016-10-28 01:36:01 +0200

    made inventory merging basically work

--- registration-system/view/signups/game2/js/inventory.js
+++ registration-system/view/signups/game2/js/inventory.js
@@ -3,13 +3,22 @@ class Inventory {
     constructor() {
         this.possibleItems = {
             cheese: {
-                name: 'Käse'
+                name: 'Käse',
+                merge: {
+                    add: 'key',
+                    produce: 'salt'
+                }
             },
             key: {
-                name: 'Schlüssel'
+                name: 'Schlüssel',
+                merge: {
+                    add: 'cheese',
+                    produce: 'salt'
+                }
             },
             salt: {
-                name: 'Salzstreuer'
+                name: 'Salzstreuer',
+                max: 2
             }
         };
 
@@ -18,53 +27,80 @@ class Inventory {
         this.inventoryBucket = document.getElementById('inventory-bucket');
     }
 
+    hasItem(name) {
+        return this.foundItems.indexOf(name) >= 0;
+    }
+
+    hasItemMaxCount(name) {
+        if (!this.hasItem(name)) return false;
+        if ((typeof this.possibleItems[name] !== 'object') || !('max' in this.possibleItems[name]))
+            return this.hasItem(name);
+        var cnt = 0;
+        this.foundItems.forEach((val, i) => {
+            if (val == name) cnt++;
+        });
+        return cnt >= this.possibleItems[name]['max'];
+    }
+
+    isItemPossible(name) {
+        return name in this.possibleItems;
+    }
+
     addItem(name) {
-        var itemImg = new Image();
-        itemImg.src = Environment.fapi.resolvePath('graphics/inventory/' + name + '.png');
-        itemImg.id = 'inventory-item-img-' + name;
-        itemImg.setAttribute('draggable', true);
-        itemImg.addEventListener('dragstart', Inventory.dragDragStartHandler, false);
+        if (this.isItemPossible(name) && !this.hasItemMaxCount(name)) {
+            var itemImg = new Image();
+            itemImg.src = Environment.fapi.resolvePath('graphics/inventory/' + name + '.png');
+            itemImg.id = 'inventory-item-img-' + name;
+            itemImg.setAttribute('draggable', true);
+            itemImg.addEventListener('dragstart', Inventory.dragDragStartHandler, false);
 
-        var tooltip = document.createElement('span');
-        tooltip.appendChild(document.createTextNode(this.possibleItems[name].name));
-        tooltip.className = 'inventory-elem-tooltip';
+            var tooltip = document.createElement('span');
+            tooltip.appendChild(document.createTextNode(this.possibleItems[name].name));
+            tooltip.className = 'inventory-elem-tooltip';
 
-        // wrap image in span
-        var itemSpan = document.createElement('span');
-        itemSpan.className = 'inventory-elem';
-        itemImg.id = 'inventory-item-' + name;
-        itemSpan.addEventListener('drop', Inventory.dragDropHandler, false);
-        itemSpan.addEventListener('dragleave', Inventory.dragDragLeaveHandler, false);
-        itemSpan.addEventListener('dragover', Inventory.dragDragOverHandler, false);
+            // wrap image in span
+            var itemSpan = document.createElement('span');
+            itemSpan.className = 'inventory-elem';
+            itemSpan.id = 'inventory-item-' + name;
+            itemSpan.addEventListener('drop', Inventory.dragDropHandler, false);
+            itemSpan.addEventListener('dragleave', Inventory.dragDragLeaveHandler, false);
+            itemSpan.addEventListener('dragover', Inventory.dragDragOverHandler, false);
 
-        itemSpan.appendChild(tooltip);
-        itemSpan.appendChild(itemImg);
+            itemSpan.appendChild(tooltip);
+            itemSpan.appendChild(itemImg);
 
-        // add to bucket
-        this.inventoryBucket.appendChild(itemSpan);
+            // add to bucket
+            this.inventoryBucket.appendChild(itemSpan);
+            this.foundItems.push(name);
+        }
     }
 
     mergeItems(nameA, nameB) {
-
+        if ((typeof this.possibleItems[nameA] === 'object') &&
+            ('merge' in this.possibleItems[nameA]) &&
+            (this.possibleItems[nameA]['merge']['add'] == nameB)) {
+            this.addItem(this.possibleItems[nameA]['merge']['produce']);
+        }
     }
 
     static dragDragStartHandler(ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
+        ev.dataTransfer.setData('text', ev.target.id);
     }
 
     static dragDropHandler(ev) {
         ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
+        ev.currentTarget.style.background = 'initial';
+        inv.mergeItems(ev.dataTransfer.getData('text').replace('inventory-item-img-',''),
+            ev.currentTarget.id.replace('inventory-item-',''));
     }
 
     static dragDragOverHandler(ev) {
         ev.preventDefault();
-        ev.currentTarget.style.background = "red";
+        ev.currentTarget.style.background = 'red';
     }
 
     static dragDragLeaveHandler(ev) {
         ev.preventDefault();
-        ev.currentTarget.style.background = "inherit";
+        ev.currentTarget.style.background = 'initial';
     }
 }
